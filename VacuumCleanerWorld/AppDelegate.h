dfde0dd8682f8c583207ddd10e9de49ddc58bea6
AntonPetrovMak <iMac309@imac309.local>
2016-03-05 17:13:29 +0200

    Initial Commit

---
+++ VacuumCleanerWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VacuumCleanerWorld
+//
+//  Created by iMac309 on 05.03.16.
+//  Copyright © 2016 iMac309. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VacuumCleanerWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VacuumCleanerWorld
+//
+//  Created by iMac309 on 05.03.16.
+//  Copyright © 2016 iMac309. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
